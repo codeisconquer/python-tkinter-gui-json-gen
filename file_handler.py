35b0963b9db26538f1fad84b0079e43e52c2df14
Joern Viebrock <joernviebrock@gmail.com>
2022-01-15 23:57:50 +0100

    Insert first working version

---
+++ file_handler.py
@@ -0,0 +1,24 @@
+import json
+import os
+import pandas as pd
+
+
+class FileHandler:
+    json_file_name = "list_of_teasers.json"
+
+    def add_to_json_file(self, new_entry):
+        lod = self.read_json_file()
+        lod.append(new_entry)
+
+        with open(self.json_file_name, 'w') as fout:
+            json.dump(lod, fout)
+
+    def read_json_file(self):
+        lod = []
+        if not os.path.isfile(self.json_file_name):
+            return lod
+
+        with open(self.json_file_name, encoding='utf-8') as data_file:
+            data = json.loads(data_file.read())
+            lod = data
+        return lod
---
+++ main.py
@@ -0,0 +1,53 @@
+import json
+from tkinter import *
+from functools import partial
+
+from file_handler import FileHandler
+
+
+class Form:
+    form_title = 'Teaser creator'
+    form_size = '400x150'
+    blueprint_file = "form_construction.json"
+    blueprint = []
+
+    def __init__(self):
+        # window
+        self.tkWindow = Tk()
+        self.tkWindow.geometry(self.form_size)
+        self.tkWindow.title(self.form_title)
+
+        self.create()
+
+    def load_blueprint(self):
+        with open(self.blueprint_file, encoding='utf-8') as data_file:
+            self.blueprint = json.loads(data_file.read())
+
+    def save_form(self, submit_data):
+        d = {}
+        for n in submit_data:
+            d[n] = submit_data[n].get()
+        fh = FileHandler()
+        fh.add_to_json_file(d)
+        print("saved " + d['headline'])
+        return
+
+    def create(self):
+        self.load_blueprint()
+        row_counter = 0
+        element_inputs = {}
+        for element in self.blueprint:
+            Label(self.tkWindow, text=element['label']).grid(row=row_counter, column=0)
+            element_inputs[element['name']] = StringVar()
+            Entry(self.tkWindow, textvariable=element_inputs[element['name']]).grid(row=row_counter, column=1)
+            row_counter = row_counter + 1
+
+        form_fields = partial(self.save_form, element_inputs)
+        Button(self.tkWindow, text="Add", command=form_fields).grid(row=row_counter, column=0)
+
+    def run(self):
+        self.tkWindow.mainloop()
+
+
+f = Form()
+f.run()
